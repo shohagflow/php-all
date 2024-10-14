802a73416d00b405c578656b68b1c55a97b6ec67
shohagflow <cseshohag1275@gmail.com>
2024-10-14 15:19:04 +0600

    mamurjor Youtub chenal PHP playlist class number -39

---
+++ assending-array.php
@@ -0,0 +1,12 @@
+<?php
+$list =array("d","c","b","a");
+
+// sort($list);
+// foreach($list as $single){
+//     echo $single."<br>";
+// }
+rsort($list);
+foreach($list as $single){
+    echo $single."<br>";
+}
+?>

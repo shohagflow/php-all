dd29fbdfa4989793adfe8ce7dc7f1614eabce18e
shohagflow <cseshohag1275@gmail.com>
2024-10-14 11:03:42 +0600

    while loop

---
+++ while.php
@@ -0,0 +1,14 @@
+<?php
+
+// function funName(){
+//     echo "Allah is  gread";
+// }
+// funName();
+$post = 30;
+$init = 2;
+while($init <= $post){
+    echo "This is my post". $init. "<br>";
+    $init++ ;
+}
+
+?>

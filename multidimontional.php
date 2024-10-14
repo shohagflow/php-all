54b151634676df02c051d7032e0e5cbd5ba8708d
shohagflow <cseshohag1275@gmail.com>
2024-10-14 14:21:17 +0600

    Heare is 3 dimontional array

---
+++ multidimontional.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$personalInfo = array(
+    "name" => "Shohag Rana",
+    "address" => array(
+        "house_no" => "Chalkpara-9",
+        "road_no" => "7",
+        "area" => array(
+            "one" => 120
+        ),
+    ),
+);
+// echo $personalInfo["address"]["area"]["one"];
+echo $personalInfo["name"];
+?>

e766967afa3b01de14b52e2821c27d2c31971da0
shohagflow <cseshohag1275@gmail.com>
2024-10-14 15:38:59 +0600

    class number 42

---
+++ superglobal.php
@@ -0,0 +1,17 @@
+<?php
+// You have to mamorise 100% it is very importent
+            // $GLOBALS
+// $_SERVER
+// $_REQUEST
+// $_POST
+// $_GET
+// $_FILES
+// $_ENV
+// $_SESSION
+// $_COOKIE
+$PersonName = "Shohag Rana";
+function getName(){
+    echo $GLOBALS["PersonName"];
+}
+getName();
+?>

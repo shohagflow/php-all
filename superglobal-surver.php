ac6c6c74c050ecefd864497fb946fd93fc864591
shohagflow <cseshohag1275@gmail.com>
2024-10-14 16:01:24 +0600

    you have to mamorise all

---
+++ superglobal-surver.php
@@ -0,0 +1,11 @@
+<?php
+// echo $_SERVER["SERVER_ADDR"];
+// echo $_SERVER["SERVER_NAME"];
+// echo $_SERVER["SERVER_SOFTWARE"];
+// echo $_SERVER["SERVER_PROTOCOL"];
+// echo $_SERVER["SERVER_PORT"];
+// echo $_SERVER["REMOTE_PORT"];
+// echo $_SERVER["SCRIPT_NAME"];
+// echo $_SERVER["PHP_SELF"];
+echo $_SERVER["SERVER_NAME"];
+?>
